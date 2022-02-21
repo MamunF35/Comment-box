99807fbbf0cb2dcc119eb8fc2bb04081a71a47e0
Al Mamun <mamun043f35@gmail.com>
2022-02-21 19:55:52 +0600

    first commit

---
+++ js/test.js
@@ -0,0 +1,11 @@
+function makeItYellow() {
+    // const makeYellow = document.getElementById("make-yellow");
+    document.body.style.backgroundColor = "yellow";
+}
+
+const makeBlue=document.getElementById("make-blue");
+        makeBlue.onclick=makeItBlue;
+function makeItBlue(){
+    document.body.style.backgroundColor="blue";
+
+}
